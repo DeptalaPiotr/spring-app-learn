61986f163b9398fd5e7eb26ec9921348dfc659f8
Piotr <deptala.piotr@gmail.com>
2022-08-30 11:52:18 +0200

    initial project structure for Maven and Spring

---
+++ src/main/java/pl/deptala/piotr/java/spring/app/learn/SpringAppLearnApplication.java
@@ -0,0 +1,15 @@
+package pl.deptala.piotr.java.spring.app.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppLearnApplication.class, args);
+	}
+
+}
+// TODO: 30.08.2022
+// Analogicznie jak NoteRepositoryTest przetestować create dla AuthorEntity
