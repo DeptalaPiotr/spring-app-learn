b2ba5ed59868cad2768a5a767d5adaa4e0ddaa06
Piotr <deptala.piotr@gmail.com>
2022-09-22 18:28:45 +0200

    create test for AuthorEntity

--- src/main/java/pl/deptala/piotr/java/spring/app/learn/repository/AuthorRepository.java
+++ src/main/java/pl/deptala/piotr/java/spring/app/learn/repository/AuthorRepository.java
@@ -1,4 +1,9 @@
 package pl.deptala.piotr.java.spring.app.learn.repository;
 
-public class AuthorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.deptala.piotr.java.spring.app.learn.repository.entity.AuthorEntity;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<AuthorEntity,Long> {
 }
